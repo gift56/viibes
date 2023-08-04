4336ecca38beac63250ff01b070b2414d4b19508
gift56 <efeasiughu@gmail.com>
2023-08-04 14:37:09 -0700

    updated some stuff

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
-  )
-}
+    <nav className="flex flex-col items-start justify-start gap-8 h-screen py-5">
+      <Link href="/">
+        <div className="w-[100px] md:w-[180px] aspect-video sticky top-2 z-10 -translate-y-5">
+          <img
+            className="cursor-pointer object-contain w-full h-full"
+            src="/vibeLogo.svg"
+            alt="viibe-logo"
+          />
+        </div>
+      </Link>
+    </nav>
+  );
+};
 
-export default LeftSideBar
+export default LeftSideBar;
