f302f49d68fcea4b29d99fbff6519553195655b0
gift56 <efeasiughu@gmail.com>
2023-08-04 13:50:18 -0700

    updated some stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import "@/styles/globals.css";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR)
+    return (
+      <div className="w-full fixed bg-white z-40 flex h-screen items-center justify-center place-items-center">
+        <Image
+          width={62}
+          height={62}
+          className="rounded-full"
+          src="/vibeLogo.svg"
+          alt="logo photo"
+          layout="responsive"
+        />
+      </div>
+    );
+
+  return <Component {...pageProps} />;
 }
