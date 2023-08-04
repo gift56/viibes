ac9d03734b5d59185509893a25efc59ae36bbe4d
gift56 <efeasiughu@gmail.com>
2023-08-04 13:40:35 -0700

    updated some stuff

---
+++ utils/constants.tsx
@@ -0,0 +1,39 @@
+import { BsCodeSlash, BsEmojiSunglasses } from "react-icons/bs";
+import { IoGameController, IoFastFoodOutline } from "react-icons/io5";
+import { GiGalaxy, GiLipstick } from "react-icons/gi";
+import { FaPaw, FaMedal } from "react-icons/fa";
+
+export const topics = [
+  {
+    name: "coding",
+    icon: <BsCodeSlash />,
+  },
+  {
+    name: "comedy",
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: "gaming",
+    icon: <IoGameController />,
+  },
+  {
+    name: "food",
+    icon: <IoFastFoodOutline />,
+  },
+  {
+    name: "dance",
+    icon: <GiGalaxy />,
+  },
+  {
+    name: "beauty",
+    icon: <GiLipstick />,
+  },
+  {
+    name: "animals",
+    icon: <FaPaw />,
+  },
+  {
+    name: "sports",
+    icon: <FaMedal />,
+  },
+];
