7edb3d93a6cf031c788374ef30578a81b941a1c1
gift56 <efeasiughu@gmail.com>
2023-08-05 03:57:10 -0700

    done with left sidebar

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -8,18 +8,16 @@ const LeftSideBar = () => {
   return (
     <nav className="flex flex-col items-start justify-start gap-4 h-screen py-5">
       <Link href="/">
-        <div className="w-[100px] md:w-[180px] aspect-video sticky top-2 z-10">
-          <img
-            className="cursor-pointer object-contain w-full h-full"
-            src="/vibeLogo.svg"
-            alt="viibe-logo"
-          />
+        <div className="w-full sticky top-2 z-10 select-none">
+          <h2 className="text-2xl font-semibold lg:text-4xl">
+            <i>Viibes</i>
+          </h2>
         </div>
       </Link>
       <div className="w-full flex flex-col h-[80vh] overflow-y-auto">
         <div className="xl:border-b-2 border-gray-200 xl:pb-2">
           <Link href="/">
-            <div className="flex items-center justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded gap-3 hover:bg-primary p-3">
+            <div className="flex items-center justify-center xl:justify-start cursor-pointer font-semibold text-black rounded gap-3 hover:bg-primary p-3">
               <p className="text-2xl">
                 <AiFillHome />
               </p>
