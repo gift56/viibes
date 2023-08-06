508fe70129a63a6a7ea50f133dc0e9634a984edf
gift56 <efeasiughu@gmail.com>
2023-08-06 10:00:55 -0700

    working on main content

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 
 const Upload = () => {
-  return <div className="w-full flex items-start justify-start gap-5">Upload</div>;
+  const user = null;
+  return (
+    <div className="w-full flex items-start justify-start gap-5">
+      <img
+        src={user ? "" : "/avatar.png"}
+        alt="avatar"
+        className="w-16 h-16 object-contain rounded-full"
+      />
+    </div>
+  );
 };
 
 export default Upload;
