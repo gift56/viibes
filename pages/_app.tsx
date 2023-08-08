2df445083a0019c7696a3e31dfd2a298227cd471
gift56 <efeasiughu@gmail.com>
2023-08-08 13:38:07 -0700

    working on main content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 import "@/styles/globals.css";
-import Image from "next/image";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import Head from "next/head";
 import { LeftSideBar, RightSidebar } from "@/components";
 
@@ -36,17 +36,19 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Expore the fun in video sharing and Have a happy life"
         />
       </Head>
-      <main className="xl:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
-        <div className="lg:flex-[1] w-full border-b lg:border-none">
-          <LeftSideBar />
-        </div>
-        <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
-          <Component {...pageProps} />
-        </div>
-        <div className="lg:flex-[1] hidden lg:flex">
-          <RightSidebar />
-        </div>
-      </main>
+      <GoogleOAuthProvider clientId="">
+        <main className="xl:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
+          <div className="lg:flex-[1] w-full border-b lg:border-none">
+            <LeftSideBar />
+          </div>
+          <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
+            <Component {...pageProps} />
+          </div>
+          <div className="lg:flex-[1] hidden lg:flex">
+            <RightSidebar />
+          </div>
+        </main>
+      </GoogleOAuthProvider>
     </>
   );
 }
