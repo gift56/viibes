e09d97cf7e6e97ba659c2c88826f74cbd6c4736c
gift56 <efeasiughu@gmail.com>
2023-08-09 13:15:48 -0700

    working on upload file

--- components/Modals/UploadModal.tsx
+++ components/Modals/UploadModal.tsx
@@ -86,26 +86,47 @@ const UploadModal = ({ show, setShow, setChange }: ModalProp) => {
         show ? "flex" : "flex"
       }`}
     >
-      <motion.div
-        animate={show ? "open" : "open"}
-        variants={variants}
-        className="lg:w-[850px] w-[90%] bg-white rounded-lg flex flex-col items-start justify-between gap-3 transition-all duration-300 h-[550px] overflow-y-auto p-4"
-      >
-        <div className="w-full flex items-center justify-between gap-4">
-          <div className="flex flex-col items-start justify-start w-full">
-            <p className="text-2xl font-bold">Upload a Video</p>
-            <p className="text-md text-gray-400">Post videos to your account</p>
+      {userProfile ? (
+        <motion.div
+          animate={show ? "open" : "open"}
+          variants={variants}
+          className="lg:w-[850px] w-[90%] bg-white rounded-lg flex flex-col items-start justify-between gap-3 transition-all duration-300 h-[550px] overflow-y-auto p-4"
+        >
+          <div className="w-full flex items-center justify-between gap-4">
+            <div className="flex flex-col items-start justify-start w-full">
+              <p className="text-2xl font-bold">Upload a Video</p>
+              <p className="text-md text-gray-400">
+                Post videos to your account
+              </p>
+            </div>
+            <span
+              onClick={() => {
+                setShow(false);
+              }}
+              className="w-12 h-12 hover:bg-gray-500 flex items-center justify-center rounded-lg text-red-500 cursor-pointer hover:text-white transition-all duration-300"
+            >
+              <MdClose
+                size={20}
+                className="hover:rotate-180 transition-all duration-300"
+              />
+            </span>
           </div>
-          <span
-            onClick={() => {
-              setShow(false);
-            }}
-            className="hover:bg-gray-500 flex items-center justify-center rounded-lg text-red-500 cursor-pointer hover:rotate-180 transition-all duration-300"
-          >
-            <MdClose size={17} />
-          </span>
-        </div>
-      </motion.div>
+        </motion.div>
+      ) : (
+        <motion.div
+          animate={show ? "open" : "open"}
+          variants={variants}
+          className="w-fit bg-white rounded-lg flex items-center justify-center flex-col gap-3 transition-all duration-300 h-[550px] overflow-y-auto p-4"
+        >
+          <h2 className="text-2xl font-medium text-gray-800">
+            Connect with <b>Viibe</b>
+          </h2>
+          <p className="text-sm font-normal text-gray-600">
+            Join Viibe, the fastest and easiest way to make upload an share the
+            fun with friends and the world through videos
+          </p>
+        </motion.div>
+      )}
     </div>
   );
 };
