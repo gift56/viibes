4d470d9ca6dcdd45c4fdb18bca33abf17ae8963e
gift56 <efeasiughu@gmail.com>
2023-08-09 05:00:35 -0700

    working on upload modal

--- components/Modals/UploadModal.tsx
+++ components/Modals/UploadModal.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import axios from "axios";
+import useAuthStore from "@/store";
+import { SanityAssetDocument } from "@sanity/client";
+import { topics } from "@/utils/constants";
 
 interface ModalProp {
   show: boolean;
@@ -7,6 +13,17 @@ interface ModalProp {
 }
 
 const UploadModal = ({ show, setShow, setChange }: ModalProp) => {
+  const [isLoading, setIsloading] = useState(false);
+  const [wrongFileType, setWrongFileType] = useState(false);
+  const [videoAsset, setVideoAsset] = useState<
+    SanityAssetDocument | undefined
+  >();
+  const [caption, setCaption] = useState("");
+  const [cartegory, setCartegory] = useState(topics[0].name);
+  const [savingPost, setSavingPost] = useState(false);
+  const { userProfile } = useAuthStore();
+  const router = useRouter();
+
   return <div>UploadModal</div>;
 };
 
