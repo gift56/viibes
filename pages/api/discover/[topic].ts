9c7a4502a109f2b3327446ada99f1afe2b3a5976
gift56 <efeasiughu@gmail.com>
2023-08-11 16:51:09 -0700

    working on discover

--- pages/api/discover/[topic].ts
+++ pages/api/discover/[topic].ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { client } from "@/utils/client";
 import { topicPostsQuery } from "@/utils/queries";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
