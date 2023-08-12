75d11566e177749577cddd6a948589760092357c
gift56 <efeasiughu@gmail.com>
2023-08-11 17:32:13 -0700

    working on video detail page

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -10,28 +10,19 @@ interface LayoutProps {
 
 const MainLayout = ({ children, pageTitle, showRight }: LayoutProps) => {
   return (
-    <>
-      <Head>
-        <title>{`Viibes - ${pageTitle}`}</title>
-        <meta
-          name="description"
-          content="Expore the fun in video sharing and Have a happy life"
-        />
-      </Head>
-      <main className="xl:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
-        <div className="lg:flex-[1] w-full border-b lg:border-none">
-          <LeftSideBar />
+    <main className="xl:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
+      <div className="lg:flex-[1] w-full border-b lg:border-none">
+        <LeftSideBar />
+      </div>
+      <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
+        {children}
+      </div>
+      {showRight === true && (
+        <div className="lg:flex-[1] hidden lg:flex">
+          <RightSidebar />
         </div>
-        <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
-          {children}
-        </div>
-        {showRight === true && (
-          <div className="lg:flex-[1] hidden lg:flex">
-            <RightSidebar />
-          </div>
-        )}
-      </main>
-    </>
+      )}
+    </main>
   );
 };
 
