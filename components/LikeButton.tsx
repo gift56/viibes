40dc462ea5f3f680ff16b894db23cd510d7c5224
gift56 <efeasiughu@gmail.com>
2023-08-11 17:10:40 -0700

    working on layout

---
+++ components/LikeButton.tsx
@@ -0,0 +1,53 @@
+import useAuthStore from "@/store";
+import React, { useState, useEffect } from "react";
+import { MdFavorite } from "react-icons/md";
+
+interface IProps {
+  handleLike: () => void;
+  handleDislike: () => void;
+  likes: any[];
+}
+
+const LikeButton = ({ handleLike, handleDislike, likes }: IProps) => {
+  const [liked, setLiked] = useState(false);
+  const { userProfile }: any = useAuthStore();
+  const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+
+  useEffect(() => {
+    if (filterLikes?.length > 0) {
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [filterLikes, likes]);
+
+  return (
+    <div className="flex gap-6">
+      <div className="flex flex-col justify-center items-center mt-4 cursor-pointer">
+        {liked ? (
+          <div
+            className="bg-primary dark:bg-gray-800 rounded-full p-2 md:p-4 text-[#f51997]"
+            onClick={handleDislike}
+          >
+            <MdFavorite className="text-lg md:text-3xl" />
+          </div>
+        ) : (
+          <div
+            className="bg-gray-200 rounded-full p-2 md:p-4"
+            onClick={handleLike}
+          >
+            <MdFavorite className="text-lg md:text-3xl" />
+          </div>
+        )}
+        <p
+          className="text-md font-semibold
+        "
+        >
+          {likes?.length | 0}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LikeButton;
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+// import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from "axios";
 
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  return <></>;
 };
 
 export default VideoDetail;
