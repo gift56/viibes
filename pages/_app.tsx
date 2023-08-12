e4f7c12cd870448c5a4ce9d0a69c53a255eabb34
gift56 <efeasiughu@gmail.com>
2023-08-11 17:33:32 -0700

    working on video detail page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Viibes - </title>
+        <title>
+          Viibes - Expore the fun in video sharing and Have a happy life
+        </title>
         <meta
           name="description"
           content="Expore the fun in video sharing and Have a happy life"
