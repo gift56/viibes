2126ed4fa969857d7c63eb79ccd621400e2cbf9d
gift56 <efeasiughu@gmail.com>
2023-08-11 17:57:55 -0700

    working on video detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -80,8 +80,8 @@ const VideoDetail = ({ postDetails }: PostProps) => {
     );
 
   return (
-    <div className="flex w-full bg-white flex-wrap lg:flex-nowrap">
-      <div className="relative flex-2 w-full lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
+    <div className="flex w-full bg-white flex-wrap lg:flex-nowrap lg:h-screen lg:overflow-hidden">
+      <div className="relative flex-[2] w-full lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
           <p className="cursor-pointer" onClick={() => router.back()}>
             <MdOutlineCancel className="text-white text-[35px]" />
@@ -117,8 +117,8 @@ const VideoDetail = ({ postDetails }: PostProps) => {
           )}
         </div>
       </div>
-      <div>
-        <div className="relative w-[100vw] lg:w-[700px]">
+      <div className="lg:h-screen overflow-y-auto bg-red-500 w-full lg:flex-[1]">
+        <div className="relative lg:w-[500px] w-full">
           <div className="mt-10">
             <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
               <div className="ml-4 md:20 md:h-20 w-16 h-16">
