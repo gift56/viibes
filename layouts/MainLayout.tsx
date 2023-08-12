1ab8b591bc6d980f4c903da7d91b3405a00640ca
gift56 <efeasiughu@gmail.com>
2023-08-11 17:02:00 -0700

    working on layout

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -7,7 +7,6 @@ interface LayoutProps {
   showRight: boolean;
   pageTitle: string;
 }
-// Expore the fun in video sharing and Have a happy life
 
 const MainLayout = ({ children, pageTitle, showRight }: LayoutProps) => {
   return (
@@ -22,6 +21,9 @@ const MainLayout = ({ children, pageTitle, showRight }: LayoutProps) => {
       <main className="xl:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
         <div className="lg:flex-[1] w-full border-b lg:border-none">
           <LeftSideBar />
+        </div>
+        <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
+          {children}
         </div>
         {showRight === true && (
           <div className="lg:flex-[1] hidden lg:flex">
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,36 @@ import React from "react";
 import axios from "axios";
 import { Video } from "@/types";
 import { NoResult, Upload, VideoCard } from "@/components";
-
+import MainLayout from "@/layouts/MainLayout";
 
 interface VideoProps {
   videos: Video[];
 }
 
 const Homepage = ({ videos }: VideoProps) => {
-
   return (
-    <div className="w-full flex flex-col items-start gap-4 h-screen overflow-y-auto overflow-x-hidden scrollbar-hide">
-      <h2 className="w-full text-xl font-bold sticky top-0 bg-white pt-6 px-4">
-        Home
-      </h2>
-      <div className="flex flex-col gap-4 videos h-full w-full overflow-y-auto">
-        <div className="w-full">
-          <Upload />
+    <MainLayout
+      pageTitle="Expore the fun in video sharing and Have a happy life"
+      showRight={true}
+    >
+      <div className="w-full flex flex-col items-start gap-4 h-screen overflow-y-auto overflow-x-hidden scrollbar-hide">
+        <h2 className="w-full text-xl font-bold sticky top-0 bg-white pt-6 px-4">
+          Home
+        </h2>
+        <div className="flex flex-col gap-4 videos h-full w-full overflow-y-auto">
+          <div className="w-full">
+            <Upload />
+          </div>
+          {videos.length > 0 ? (
+            videos.map((video: Video) => (
+              <VideoCard post={video} key={video._id} />
+            ))
+          ) : (
+            <NoResult text="No Videos Found!" />
+          )}
         </div>
-        {videos.length > 0 ? (
-          videos.map((video: Video) => (
-            <VideoCard post={video} key={video._id} />
-          ))
-        ) : (
-          <NoResult text="No Videos Found!" />
-        )}
       </div>
-    </div>
+    </MainLayout>
   );
 };
 
