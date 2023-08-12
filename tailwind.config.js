cb7b87f276c0c14448643b39b801e86a1a945295
gift56 <efeasiughu@gmail.com>
2023-08-11 17:53:11 -0700

    working on video detail page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         cardShad: "0px 14px 34px rgba(0, 0, 0, 0.05)",
         paginationShad: "0px 48px 56px -25px rgba(100, 112, 122, 0.15)",
       },
+      backgroundImage: {
+        "blurred-img":
+          "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsaaJ7s4lqcBF4IDROVPzrlL5fexcwRmDlnuEYQenWTt1DejFY5kmYDref2a0Hp2eE4aw&usqp=CAU')",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
