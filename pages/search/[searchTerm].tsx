9bb157d7c80a9fe436a2104daff2d0a0c23ef30b
gift56 <efeasiughu@gmail.com>
2023-08-13 17:27:22 -0700

    working on profile page

---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,21 @@
+import { BASE_URL } from "@/utils";
+import axios from "axios";
+import React from "react";
+
+const Searchpage = () => {
+  return <div>Searchpage</div>;
+};
+
+export const getServerSideProps = async ({
+  params: { searchTerm },
+}: {
+  params: { searchTerm: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+
+  return {
+    props: { videos: res.data },
+  };
+};
+
+export default Searchpage;
