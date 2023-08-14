a34a7d7a651be95b19ba990ab0947f20733cb88c
gift56 <efeasiughu@gmail.com>
2023-08-13 17:35:15 -0700

    working on profile page

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -4,6 +4,7 @@ import { BASE_URL } from "@/utils";
 import axios from "axios";
 import { useRouter } from "next/router";
 import useAuthStore from "@/store";
+import MainLayout from "@/layouts/MainLayout";
 
 interface SearchProps {
   videos: Video[];
@@ -15,19 +16,19 @@ const Searchpage = ({ videos }: SearchProps) => {
   const { searchTerm }: any = router.query;
   const { allUsers } = useAuthStore();
 
-  const Accounts = isAccount
-    ? "border-b-2 border-black"
-    : "text-gray-400";
+  const Accounts = isAccount ? "border-b-2 border-black" : "text-gray-400";
 
-  const isVideos = !isAccount
-    ? "border-b-2 border-black"
-    : "text-gray-400";
+  const isVideos = !isAccount ? "border-b-2 border-black" : "text-gray-400";
 
   const searchedAccounts = allUsers.filter((user: IUser) =>
     user.userName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  return <div>Searchpage</div>;
+  return (
+    <MainLayout showRight={false}>
+      <div className="w-full px-5"></div>
+    </MainLayout>
+  );
 };
 
 export const getServerSideProps = async ({
